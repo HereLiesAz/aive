9768a51e8b84b2c3593d8b1a0d972ee4bdc44e9f
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-16 16:33:08 -0500

    Route Add-ons to Azphalt Store

--- shared/src/commonMain/kotlin/com/hereliesaz/geministrator/ControlRoomUi.kt
+++ shared/src/commonMain/kotlin/com/hereliesaz/geministrator/ControlRoomUi.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.hereliesaz.geministrator.azphalt.AzphaltStoreService
 import com.hereliesaz.geministrator.domain.Project
 import com.hereliesaz.geministrator.domain.RepositoryRef
 import com.hereliesaz.geministrator.domain.RepositorySource
@@ -119,6 +120,7 @@ fun ControlRoom(
     onDisconnectRepositoryService: (String) -> Unit = {},
     onReconfigureProvider: (String) -> Unit = {},
     onDisconnectProvider: (String) -> Unit = {},
+    azphaltStoreService: AzphaltStoreService? = null,
     compact: Boolean,
     contentPadding: PaddingValues,
     runtimeState: ApplicationRuntimeState,
@@ -170,6 +172,7 @@ fun ControlRoom(
                     onDisconnectRepositoryService = onDisconnectRepositoryService,
                     onReconfigureProvider = onReconfigureProvider,
                     onDisconnectProvider = onDisconnectProvider,
+                    azphaltStoreService = azphaltStoreService,
                     connectedProviderIds = connectedProviderIds,
                     modifier = Modifier.weight(1f),
                     compact = true,
@@ -232,6 +235,7 @@ fun ControlRoom(
                     onDisconnectRepositoryService = onDisconnectRepositoryService,
                     onReconfigureProvider = onReconfigureProvider,
                     onDisconnectProvider = onDisconnectProvider,
+                    azphaltStoreService = azphaltStoreService,
                     connectedProviderIds = connectedProviderIds,
                     modifier = Modifier.weight(1f),
                     runtimeState = runtimeState,
@@ -372,6 +376,7 @@ private fun MainDestination(
     onDisconnectRepositoryService: (String) -> Unit,
     onReconfigureProvider: (String) -> Unit = {},
     onDisconnectProvider: (String) -> Unit = {},
+    azphaltStoreService: AzphaltStoreService? = null,
     connectedProviderIds: Set<String>,
     modifier: Modifier = Modifier,
     compact: Boolean = false,
@@ -422,13 +427,9 @@ private fun MainDestination(
                 onDisconnectService = onDisconnectRepositoryService,
                 modifier = Modifier.fillMaxSize(),
             )
-            ControlRoomDestination.AddOns -> com.hereliesaz.geministrator.addons.AddonHostScreen(
-                installations = emptyList(),
-                onInstall = { _, _ -> },
-                onRemove = {},
-                onEnableDisable = { _, _ -> },
-                onAddAgentsToCompany = {},
-                modifier = Modifier.fillMaxSize()
+            ControlRoomDestination.AddOns -> AzphaltStoreScreen(
+                service = azphaltStoreService,
+                modifier = Modifier.fillMaxSize(),
             )
             ControlRoomDestination.Settings -> ProviderSettingsScreen(
                 connectedProviderIds = connectedProviderIds,
