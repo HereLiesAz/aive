7830c64028649d8124f4609316c3e46431fb2478
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-18 23:36:09 -0500

    Delegate installed Gemini enablement to distribution flavor

--- androidApp/src/main/kotlin/com/hereliesaz/aive/MainActivity.kt
+++ androidApp/src/main/kotlin/com/hereliesaz/aive/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hereliesaz.aive
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
 import android.os.Build
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -224,7 +223,7 @@ class MainActivity : ComponentActivity() {
                             installedGeminiReady = InstalledGeminiTextGenerationApi.isAvailable(this)
                             configuringProviderId = null
                             if (!installedGeminiReady) {
-                                startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+                                InstalledGeminiPreference.requestEnable(this)
                             }
                         },
                         onUseApiKey = { configuringGeminiApiKey = true },
