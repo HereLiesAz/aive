8880a75d4803ed89cd3bf6ef605e39be249dae89
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-19 23:39:50 -0500

    Import generated detached arm resources

--- shared/src/commonMain/kotlin/com/hereliesaz/geministrator/NodeArmAssets.kt
+++ shared/src/commonMain/kotlin/com/hereliesaz/geministrator/NodeArmAssets.kt
@@ -1,6 +1,6 @@
 package com.hereliesaz.geministrator
 
-import com.hereliesaz.geministrator.resources.Res
+import com.hereliesaz.geministrator.resources.*
 import org.jetbrains.compose.resources.DrawableResource
 
 internal enum class NodeArmVariant {
